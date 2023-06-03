b9d1013607214c13b3e65b306f9979e674bfaaf3
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-06-03 14:31:59 -0300

    Criação de Construtores da classe Cliente.

--- BancoDIgital/src/bancodigital/Cliente.java
+++ BancoDIgital/src/bancodigital/Cliente.java
@@ -9,5 +9,47 @@ package bancodigital;
  * @author MatheusWP
  */
 public class Cliente {
-    
+
+    private String nome;
+    private String cpf;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
