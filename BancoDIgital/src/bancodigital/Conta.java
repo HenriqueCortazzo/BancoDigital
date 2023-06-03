df0cf940e6a3a10dcc12a980d3cac33f49545673
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-06-03 14:35:41 -0300

    Criação de Atributos e metodos da Classe.

--- BancoDIgital/src/bancodigital/Conta.java
+++ BancoDIgital/src/bancodigital/Conta.java
@@ -9,5 +9,38 @@ package bancodigital;
  * @author MatheusWP
  */
 public class Conta {
+    private int agencia;
+    private int numero;
+    private double saldo;
     
+    public void sacar(){
+    
+    }
+    public void depositar(){
+    
+    }
+    public void transferir(){
+    
+    }
+
+    /**
+     * @return the agencia
+     */
+    public int getAgencia() {
+        return agencia;
+    }
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
 }
