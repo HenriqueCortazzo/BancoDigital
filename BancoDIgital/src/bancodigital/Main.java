840d4d575f91ce3e677dfbe1ae1533d4a36c87cb
Matheus Henrique <110773304+HenriqueCortazzo@users.noreply.github.com>
2023-06-03 16:18:14 -0300

    Atualização e implementação Classe Main

--- BancoDIgital/src/bancodigital/Main.java
+++ BancoDIgital/src/bancodigital/Main.java
@@ -1,16 +1,20 @@
 package bancodigital;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        Conta c = null;
-        Conta c1 = new ContaCorrente(new Cliente("Jorge", "123456788-22"));
+        Conta userClient = null;
+        Conta c1 = new ContaPoupanca(new Cliente("Jorge", "123456788-22"));
 
-        System.out.println("Banco XXL");
-        System.out.println("Criacao de Conta");
+        JOptionPane.showMessageDialog(null, "               Seja Bem Vindo ao Banco Vang! \n Estamos inicializando o sistema em seu Terminal. \n                           Por favor aguarde.\n");
+
+        System.out.println("Olá, seja Bem Vindo! \nEstavámos esperando por você!\n");
+
+        System.out.println("Criação de Conta");
         System.out.print("Digite o seu nome:\n");
 
         String nome = ler.nextLine();
@@ -24,13 +28,59 @@ public class Main {
 
         switch (ler.nextInt()) {
             case 1:
-                c = new ContaCorrente(cliente);
-                System.out.println("\nO que você deseja?");
+                userClient = new ContaCorrente(cliente);
+                System.out.println("\nVocê selecionou a opção de Conta Corrente.");
+                System.out.println("Estamos Inicializando o Sistema.");
+                System.out.println("Por favor, aguarde.");
                 break;
             case 2:
-                c = new ContaPoupanca(cliente);
-                System.out.println("\nO que você deseja?");
+                userClient = new ContaPoupanca(cliente);
+                System.out.println("\nVocê selecionou a opção de Conta Poupança.");
+                System.out.println("Estamos Inicializando o Sistema.");
+                System.out.println("Por favor, aguarde.");
         }
 
+        for (int i = 0; i != 5;) {
+            System.out.println();
+            System.out.println("O que você deseja?");
+            System.out.println("Selecione um número");
+            System.out.println("1-Depositar \n2-Sacar \n3-Transferir \n4-Extrato \n5-Encerrar atividade");
+            int acaoCliente = ler.nextInt();
+            i = acaoCliente;
+            switch (acaoCliente) {
+                case 1:
+                    System.out.print("Digite o valor do deposito:");
+                    userClient.depositar(ler.nextDouble());
+                    System.out.println("Saldo: R$" + userClient.getSaldo());
+                    break;
+                case 2:
+                    System.out.print("Digite o valor do saque:");
+                    Double valorSaque = ler.nextDouble();
+                    if (valorSaque > userClient.getSaldo()) {
+
+                        System.out.println("Saldo insuficiente!" + "\n Saldo atual: "
+                                + userClient.getSaldo() + "\n Valor do saque: " + valorSaque);
+                    } else {
+
+                        Double temp1 = userClient.getSaldo();
+                        userClient.sacar(valorSaque);
+                        System.out.println("Saldo Antigo: R$" + temp1 + "\n Saldo Atual: R$"
+                                + userClient.getSaldo());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Digite o valor da transferência para " + c1.getCliente());
+                    userClient.transferir(ler.nextDouble(), c1);
+                    System.out.println("Transação concluida!");
+                    break;
+                case 4:
+                    userClient.imprimirExtrato();
+                    break;
+                case 5:
+                    userClient.imprimirExtrato();
+                    break;
+            }
+
+        }
     }
 }
